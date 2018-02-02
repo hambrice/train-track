6f7719b5a10339b94b53c54d1b84cbccfc985343
Brad Hambrice <hambrice@uab.edu>
2018-02-02 21:32:18 +0000

    built Users db

--- db/migrate/002_create_users.rb
+++ db/migrate/002_create_users.rb
@@ -1,10 +1,11 @@
-class CreateTrainers < ActiveRecord::Migration[4.2]
+class CreateUsers < ActiveRecord::Migration[4.2]
   def change
-    create_table :trainers do |t|
+    create_table :users do |t|
       t.string :first_name
       t.string :last_name
       t.string :email
       t.string :password_digest
+      t.integer :trainer_id
     end
   end
 end
