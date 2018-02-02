fd208d87a67ca5e75d7917c4eb49d55b4c3a85f0
Brad Hambrice <hambrice@uab.edu>
2018-02-02 21:55:39 +0000

    fixed clients db

--- db/migrate/002_create_clients.rb
+++ db/migrate/002_create_clients.rb
@@ -1,11 +1,10 @@
-class CreateUsers < ActiveRecord::Migration[4.2]
+class CreateClients < ActiveRecord::Migration[4.2]
   def change
-    create_table :users do |t|
+    create_table :clients do |t|
       t.string :first_name
       t.string :last_name
       t.string :email
       t.string :password_digest
-      t.integer :trainer_id
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
-
-  create_table "trainers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "password_digest"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
