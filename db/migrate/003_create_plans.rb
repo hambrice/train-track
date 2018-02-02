877fe7644a5a77665d93e978f7793da0d2d8d183
Brad Hambrice <hambrice@uab.edu>
2018-02-02 21:53:22 +0000

    built Plans db

---
+++ db/migrate/003_create_plans.rb
@@ -0,0 +1,9 @@
+class CreatePlans < ActiveRecord::Migration[4.2]
+  def change
+    create_table :plans do |t|
+      t.string :title
+      t.integer :trainer_id
+      t.integer :client_id
+    end
+  end
+end
