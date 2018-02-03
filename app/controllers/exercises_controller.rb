29c40392af1564eb8e062b2e8c7da380e499a79b
Brad Hambrice <hambrice@uab.edu>
2018-02-03 05:43:50 +0000

    built exercises/new and /:id actions

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -3,4 +3,14 @@ class ExercisesController < ApplicationController
   get '/exercises/new' do
     erb :'/exercises/new'
   end
+
+  post '/exercises/new' do
+    @exercise = Exercise.create(title: params["name"], description: params["description"], sets: params["sets"], reps: params["reps"])
+    redirect "/exercises/#{@exercise.id}"
+  end
+
+  get '/exercises/:id' do
+    @exercise = Exercise.find(params[:id])
+    erb :'/exercises/show'
+  end
 end
