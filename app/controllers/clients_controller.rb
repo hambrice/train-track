f7dca799ce5c487365be0a55048c63ff473530b7
Brad Hambrice <hambrice@uab.edu>
2018-02-03 01:26:03 +0000

    created trainer/signup page

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -3,4 +3,11 @@ class ClientsController < ApplicationController
   get '/clients/signup' do
     erb :'/clients/signup'
   end
+
+  post '/clients/show' do
+    @client = Client.new(first_name: params["first_name"], last_name: params["last_name"], email: params["email"], password: params["password"])
+    @session = session
+    @session[:user_id] = @client.id
+    erb :'/clients/show'
+  end
 end
