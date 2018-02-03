73bad019d5cf61178f16a667db002b41737604c7
Brad Hambrice <hambrice@uab.edu>
2018-02-03 07:36:16 +0000

    fixed exercise.safe_update method

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Exercise < ActiveRecord::Base
   has_many :exercise_plans
   has_many :plans, through: :exercise_plans
@@ -7,12 +8,19 @@ class Exercise < ActiveRecord::Base
   end
 
   def safe_update(params)
-    binding.pry
-    self.update(title: params["name"]) if params["name"] != nil
-    self.update(description: params["description"]) if params["description"] != nil
-    self.update(sets: params["sets"]) if !params["sets"] != nil
-    self.update(reps: params["reps"]) if !params["rep"] != nil
-    self.update(body_region: params["body_region"]) if !params["body_region"] != nil
+    #binding.pry
+    self.class.column_names.each do |column|
+      if params[column] != "" && column != "id"
+        self.update(column.to_sym => params[column])
+      end
+    end
+    self.save
+    # self.update(description: params["description"]) if params["description"] != nil
+    # self.update(sets: params["sets"]) if !params["sets"] != nil
+    # self.update(reps: params["reps"]) if !params["rep"] != nil
+    # self.update(body_region: params["body_region"]) if !params["body_region"] != nil
   end
 
 end
+
+#exercise = Exercise.create(title: "jumprope", description: "jumprope", sets: 9, reps: 10, body_region: "back")
