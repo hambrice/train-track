3418aa66c1e38c282bdd27d88bbc69d6f6092e82
Brad Hambrice <hambrice@uab.edu>
2018-02-03 08:01:28 +0000

    create plans/new controller action

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,15 @@
 class PlansController < ApplicationController
 
+  get '/plans/new' do
+    erb :'/plans/new'
+  end
 
+  post '/plans' do
+    @plan = Plan.create(title: params["title"])
+    params["exercises"].each do |exercise|
+      @plan.exercises << Exercise.find(exercise)
+    end
+    @plan.trainer = Trainer.find(session[:user_id])
+    erb :'/plans/show'
+  end
 end
