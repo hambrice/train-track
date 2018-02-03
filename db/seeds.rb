c9813cf72603becf053977c69729e111df0b5f11
Brad Hambrice <hambrice@uab.edu>
2018-02-03 02:27:20 +0000

    fixed trainers/show page to display clients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 trainer = Trainer.create(first_name: "Brad", last_name: "Hambrice", email: "hambrice@uab.edu", password: "547623")
-trainer.clients << Client.create(first_name: "Caitlin", last_name: "Lopez", email: "lopez@uab.edu", password: "547623")
-trainer.clients << Client.create(first_name: "Lauren", last_name: "Duke", email: "hambrice@uab.edu", password: "547623")
-trainer.clients << Client.create(first_name: "Andy", last_name: "Herren", email: "hambrice@uab.edu", password: "547623")
+client1 = Client.create(first_name: "Caitlin", last_name: "Lopez", email: "lopez@uab.edu", password: "547623")
+client2 = Client.create(first_name: "Lauren", last_name: "Duke", email: "hambrice@uab.edu", password: "547623")
+client3 = Client.create(first_name: "Andy", last_name: "Herren", email: "hambrice@uab.edu", password: "547623")
+trainer.clients << client1
+trainer.clients << client2
+trainer.clients << client3
