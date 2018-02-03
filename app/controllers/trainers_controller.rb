711b79f1febb64d9772f29cc0e5b23914371721f
Brad Hambrice <hambrice@uab.edu>
2018-02-03 01:39:21 +0000

    created trainers/show page

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,4 +1,14 @@
 class TrainersController < ApplicationController
 
+  get '/trainers/signup' do
+    erb :'/trainers/signup'
+  end
+
+  post '/trainers/show' do
+    @trainer = Trainer.new(first_name: params["first_name"], last_name: params["last_name"], email: params["email"], password: params["password"])
+    @session = session
+    @session[:user_id] = @trainer.id
+    erb :'trainers/show'
+  end
 
 end
