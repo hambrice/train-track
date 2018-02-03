cf31eeaa8e1c59a8171b8a282bc06c3ffede6e2b
Brad Hambrice <hambrice@uab.edu>
2018-02-03 02:03:06 +0000

    establishing login redirect in application

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,13 +1,21 @@
 class ClientsController < ApplicationController
 
+  get '/clients/:id' do
+    @client = Client.find(params[:id])
+    erb :'/clients/show'
+  end
+  
   get '/clients/signup' do
     erb :'/clients/signup'
   end
 
   post '/clients/show' do
-    @client = Client.new(first_name: params["first_name"], last_name: params["last_name"], email: params["email"], password: params["password"])
+    @client = Client.create(first_name: params["first_name"], last_name: params["last_name"], email: params["email"], password: params["password"])
     @session = session
     @session[:user_id] = @client.id
-    erb :'/clients/show'
+    erb :'/application/login'
   end
+
+
+
 end
--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,5 +1,10 @@
 class TrainersController < ApplicationController
 
+  get '/trainers/:id' do
+    @trainer = Trainer.find(params[:id])
+    erb :'/trainers/show'
+  end
+
   get '/trainers/signup' do
     erb :'/trainers/signup'
   end
@@ -8,7 +13,8 @@ class TrainersController < ApplicationController
     @trainer = Trainer.create(first_name: params["first_name"], last_name: params["last_name"], email: params["email"], password: params["password"])
     @session = session
     @session[:user_id] = @trainer.id
-    erb :'trainers/show'
+    erb :'/application/login'
   end
+
 
 end
