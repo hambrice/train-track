3fcbcbeb884e66480eeec6b726d655655d06fe97
Brad Hambrice <hambrice@uab.edu>
2018-02-04 01:29:39 +0000

    created scheduler method for plan model

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -3,4 +3,10 @@ class Plan < ActiveRecord::Base
   belongs_to :trainer
   has_many :exercise_plans
   has_many :exercises, through: :exercise_plans
+
+  def scheduler(day)
+    self.exercises.collect do |exercise|
+      exercise if exercise.schedule.include?(day)
+    end
+  end
 end
