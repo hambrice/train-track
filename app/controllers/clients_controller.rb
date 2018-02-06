9009eefacb027c0a51e44f372d01a64430eda7fb
Brad Hambrice <hambrice@uab.edu>
2018-02-06 02:34:37 +0000

    fixed issues with registering new clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -5,15 +5,12 @@ class ClientsController < ApplicationController
   end
 
   post '/clients/new' do
-    @client = Client.create(email: params["email"])
+    @client = Client.create(first_name: "user", last_name: "user", email: params["email"], password: "user")
+    @client.update(first_name: nil, last_name: nil, email: params["email"], password: nil)
     @trainer = Trainer.find(session[:user_id])
     @trainer.clients << @client
+    @trainer.save
     redirect "/trainers/#{@trainer.id}"
-  end
-
-  get '/clients/:id' do
-    @client = Client.find(params[:id])
-    erb :'/clients/show'
   end
 
   get '/clients/signup' do
@@ -22,12 +19,21 @@ class ClientsController < ApplicationController
 
   post '/clients/show' do
     @client = Client.find_by(email: params["email"])
+    if @client == nil
+      redirect '/clients/failure'
+    end
     @client.update(first_name: params["first_name"], last_name: params["last_name"], password: params["password"])
     session[:user_id] = @client.id
     redirect "/clients/#{@client.id}"
   end
 
+  get '/clients/failure' do
+    erb :'/clients/failure'
+  end
 
-
+  get '/clients/:id' do
+    @client = Client.find(params[:id])
+    erb :'/clients/show'
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,12 @@ trainer = Trainer.create(first_name: "Brad", last_name: "Hambrice", email: "hamb
 client1 = Client.create(first_name: "Caitlin", last_name: "Lopez", email: "lopez@uab.edu", password: "547623")
 client2 = Client.create(first_name: "Lauren", last_name: "Duke", email: "hambrice@uab.edu", password: "547623")
 client3 = Client.create(first_name: "Andy", last_name: "Herren", email: "hambrice@uab.edu", password: "547623")
+client4 = Client.create(email: "hambrice@uab.edu")
+
 trainer.clients << client1
 trainer.clients << client2
 trainer.clients << client3
+trainer.clients << client4
 Exercise.create(title: "Pushups", description: "do them", sets:3, reps: 8, body_region: "Chest")
 Exercise.create(title: "Pullups", description: "do plenty", sets:3, reps: 8, body_region: "Back")
 Exercise.create(title: "Bicep Curls", description: "work hard", sets:3, reps: 8, body_region: "Biceps")
